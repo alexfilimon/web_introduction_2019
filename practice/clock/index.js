19ebb674adbd5d69f898fd936fc650f020956200
Alexander Filimonov <as_filimon@mail.ru>
2019-11-30 14:10:17 +0300

    Добавил код для секунд

--- practice/clock/index.js
+++ practice/clock/index.js
@@ -1,1 +1,13 @@
 // $(".wrap .digit").addClass("one");
+
+let currentDigit = 0
+
+let digits = ["zero", "one", "two", "three", 
+    "four", "five", "six", "seven", "eight", "nine"]
+
+setInterval(function() {
+    currentDigit = (currentDigit + 1) % 10;
+    $(".wrap .digit")
+        .removeClass(digits)
+        .addClass(digits[currentDigit])
+}, 1000);
